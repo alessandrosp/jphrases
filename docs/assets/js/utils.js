885b160aa6daf187e3174b2a0aa2d19847909f4e
Alessandro <9991341+annoys-parrot@users.noreply.github.com>
2019-12-25 22:31:03 +0000

    Update utils.js

--- docs/assets/js/utils.js
+++ docs/assets/js/utils.js
@@ -1,6 +1,12 @@
 var utils = {
 
-    testLog: function() {
-    	console.log('This is just a test.')
-    },
+  testLog: function() {
+    console.log('This is just a test.')
+  },
+
+  showExplanation: function() {
+    $(".showable").each(function() {
+      this.css("visibility", "visible");
+    });
+  },
 };
