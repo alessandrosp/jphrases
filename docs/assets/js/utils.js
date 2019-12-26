39f87f4090ce5ea9cfe5420d43216e8b591e2c22
Alessandro <9991341+annoys-parrot@users.noreply.github.com>
2019-12-26 08:53:00 +0000

    Update utils.js

--- docs/assets/js/utils.js
+++ docs/assets/js/utils.js
@@ -9,4 +9,13 @@ var utils = {
       $(this).css('display', 'block');
     });
   },
+
 };
+
+$(window).keydown(function(event) {
+  // Get more information here: https://keycode.info/
+  // 69: E
+  if (event.which == 69) {
+    utils.showExplanation();
+  }
+});
