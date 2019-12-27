e7397db4321a65ba29da127e1d228eb4f1bf70bd
Alessandro <9991341+annoys-parrot@users.noreply.github.com>
2019-12-27 17:27:32 +0000

    Update utils.js

--- docs/assets/js/utils.js
+++ docs/assets/js/utils.js
@@ -1,16 +1,29 @@
 var utils = {
 
+  showExplanation: function() {
+    $('.showable').each(function() {
+      if ($(this).parents('.phrase-active').length) {
+        $(this).css('display', 'block');
+      };
+    });
+    $('.explain').each(function() {
+      if ($(this).parents('.phrase-active').length) {
+        $(this).css('display', 'none');
+      };
+    });
+  },
+  
   testLog: function() {
     console.log('This is just a test.')
   },
-
-  showExplanation: function() {
-    $('.showable').each(function() {
-      $(this).css('display', 'block');
-    });
-    $('.explain').each(function() {
-      $(this).css('display', 'none');
-    });
+  
+  markCorrect: function() {
+    let active = $('.phrase-active').first();
+    let firstInactive = $('.phrase-inactive').first();
+    active.removeClass('phrase-active');
+    active.addClass('phrase-inactive');
+    firstInactive.removeClass('phrase-inactive');
+    firstInactive.addClass('phrase-active');
   },
 
 };
@@ -20,5 +33,7 @@ $(window).keydown(function(event) {
   // 69: E
   if (event.which == 69) {
     utils.showExplanation();
-  }
+  } else if (event.which == 82) {
+    utils.markCorrect();
+  } 
 });
